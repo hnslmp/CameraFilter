9575fcbe4eb9053efa2a490753bc84ec546e7821
hnslmp <ancemo14@gmail.com>
2022-07-15 22:49:39 +0700

    First Commit

---
+++ CameraFilter/FilterService.swift
@@ -0,0 +1,44 @@
+//
+//  FilterSwift.swift
+//  CameraFilter
+//
+//  Created by Hansel Matthew on 15/07/22.
+//
+
+import UIKit
+import RxSwift
+import CoreImage
+
+class FilterService {
+    
+    private var context: CIContext
+    
+    init(){
+        self.context = CIContext()
+    }
+    
+    func applyFilter(to inputImage: UIImage) -> Observable<UIImage> {
+        
+        return Observable<UIImage>.create { observer in
+            
+            self.applyFilter(to: inputImage) { filteredImage in
+                observer.onNext(filteredImage)
+            }
+            return Disposables.create()
+        }
+    }
+    
+    private func applyFilter(to inputImage: UIImage, completion: @escaping((UIImage) -> ())) {
+        
+        let filter = CIFilter(name: "CICMYKHalftone")
+        filter?.setValue(5.0, forKey: kCIInputWidthKey)
+        
+        if let sourceImage = CIImage(image: inputImage) {
+            filter?.setValue(sourceImage, forKey: kCIInputImageKey)
+            if let cgimg = self.context.createCGImage(filter!.outputImage!, from: (filter?.outputImage!.extent)!){
+                let processedImage = UIImage(cgImage: cgimg, scale: inputImage.scale, orientation: inputImage.imageOrientation)
+                completion(processedImage)
+            }
+        }
+    }
+}
---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Hansel Matthew on 15/07/22.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell{
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
